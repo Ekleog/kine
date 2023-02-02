7ef2372dba9cc4eb0181d4b5d997e2be173a2852
Léo Gaspard <leo@gaspard.io>
2023-02-02 18:10:12 +0100

    Merge almost-empty file into lib.rs

--- kine-core/src/lib.rs
+++ kine-core/src/lib.rs
@@ -2,10 +2,15 @@ mod calendar;
 mod duration;
 mod results;
 mod time;
-mod written_time;
 
 pub use calendar::{Calendar, CalendarTime};
 pub use duration::Duration;
 pub use results::{Error, Result, TimeResult, WrittenTimeResult};
 pub use time::Time;
-pub use written_time::WrittenTime;
+
+/// A date and time as indicated by a calendar
+///
+/// Anything user-visible should probably be handled using this type. However,
+/// remember that operations on it can be vastly different from operations on
+/// regular times, durations are often not commutative, etc.
+pub type WrittenTime<Cal> = <Cal as Calendar>::Time;
--- kine-core/src/written_time.rs
+++
@@ -1,8 +0,0 @@
-use crate::Calendar;
-
-/// A date and time as indicated by a calendar
-///
-/// Anything user-visible should probably be handled using this type. However,
-/// remember that operations on it can be vastly different from operations on
-/// regular times, durations are often not commutative, etc.
-pub type WrittenTime<Cal> = <Cal as Calendar>::Time;
