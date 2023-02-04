86480e46efe8f3f5a4f3eb5a02643d857f7db10c
Léo Gaspard <leo@gaspard.io>
2023-02-05 00:51:20 +0100

    Implement Calendar for kine_icu::Cal

--- kine-icu/src/lib.rs
+++ kine-icu/src/lib.rs
@@ -1,4 +1,5 @@
-use kine_core::{Calendar, CalendarTime, OffsetTime, TimeZone};
+use icu_calendar::types::{IsoSecond, NanoSecond};
+use kine_core::{Calendar, CalendarTime, OffsetTime, TimeResult, TimeZone, WrittenTimeResult};
 
 const NANOS_PER_SECS: i128 = 1_000_000_000;
 const NANOS_PER_MINS: i128 = 60 * NANOS_PER_SECS;
@@ -14,20 +15,48 @@ pub struct Time<Ca: icu_calendar::AsCalendar, Tz: TimeZone> {
     time: icu_calendar::DateTime<Ca>,
 }
 
+impl<Ca, Tz> Cal<Ca, Tz>
+where
+    Ca: Clone + icu_calendar::AsCalendar,
+    Tz: Clone + TimeZone,
+    <Tz as TimeZone>::Sigil: Clone,
+{
+    fn write_impl(
+        &self,
+        offset_time: OffsetTime<Tz::Sigil>,
+    ) -> kine_core::Result<WrittenTimeResult<Time<Ca, Tz>>> {
+        let pseudo_nanos = offset_time.as_pseudo_nanos_since_posix_epoch();
+        let extra_nanos = i128::from(offset_time.extra_nanos());
+        let minutes = pseudo_nanos / NANOS_PER_MINS;
+        let submin_pseudo_nanos = pseudo_nanos % NANOS_PER_MINS + extra_nanos;
+        let seconds = submin_pseudo_nanos / NANOS_PER_SECS;
+        let nanos = submin_pseudo_nanos % NANOS_PER_SECS;
+
+        let minutes = i32::try_from(minutes).map_err(|_| kine_core::Error::OutOfRange)?;
+        let mut res = icu_calendar::DateTime::from_minutes_since_local_unix_epoch(minutes);
+        res.time.second = IsoSecond::try_from(u8::try_from(seconds).unwrap()).unwrap();
+        res.time.nanosecond = NanoSecond::try_from(u32::try_from(nanos).unwrap()).unwrap();
+
+        Ok(WrittenTimeResult::One(Time {
+            tz: offset_time.sigil().clone(),
+            time: res.to_calendar(self.cal.clone()),
+        }))
+    }
+}
+
 impl<Ca, Tz> Calendar for Cal<Ca, Tz>
 where
-    Ca: icu_calendar::AsCalendar,
+    Ca: Clone + icu_calendar::AsCalendar,
     Tz: Clone + TimeZone,
     <Tz as TimeZone>::Sigil: Clone,
 {
     type Time = Time<Ca, Tz>;
 
-    fn write(
-        &self,
-        t: &kine_core::Time,
-    ) -> kine_core::Result<kine_core::WrittenTimeResult<Self::Time>> {
-        let offset_time = t.write(self.tz.clone());
-        todo!()
+    fn write(&self, t: &kine_core::Time) -> kine_core::Result<WrittenTimeResult<Self::Time>> {
+        Ok(match t.write(self.tz.clone())? {
+            WrittenTimeResult::One(t) => self.write_impl(t)?,
+            WrittenTimeResult::Many(t) => WrittenTimeResult::Many(self.write_impl(t)?.any()),
+        })
     }
 }
 
@@ -37,7 +66,7 @@ where
     Tz: TimeZone,
     <Tz as TimeZone>::Sigil: Clone,
 {
-    fn read(&self) -> kine_core::Result<kine_core::TimeResult> {
+    fn read(&self) -> kine_core::Result<TimeResult> {
         let time = self.time.to_calendar(icu_calendar::Iso);
         let local_mins = i128::from(time.minutes_since_local_unix_epoch());
         // TODO: revisit after https://github.com/unicode-org/icu4x/issues/3085 answered
