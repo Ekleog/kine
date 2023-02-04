8263e429af829b3dc8659e22a1a792712a7f0579
Léo Gaspard <leo@gaspard.io>
2023-02-04 02:33:20 +0100

    Base Gregorian calendar implementation off icu4x

--- kine-icu/src/timezone.rs
+++ kine-icu/src/timezone.rs
@@ -3,7 +3,7 @@ pub trait TimeZone {
     /// Write the designator of this timezone
     ///
     /// The designator is the string indicating which timezone a written date is in.
-    /// For instance, it is "Z" for UTC, "+01:00" for a fixed 1h offset, and " CET"
-    /// for Central European Time.
+    /// For instance, it is `"Z"` for UTC, `"+01:00"` for a fixed 1h offset, and
+    /// `" CET"` for Central European Time.
     fn write_designator(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
 }
