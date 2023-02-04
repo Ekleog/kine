cdc838cc9cebe9ac132ee2ab1f4a78663dccb976
Léo Gaspard <leo@gaspard.io>
2023-02-04 18:04:52 +0100

    Remove timezone from kine-icu now it's in kine-core already

--- kine-icu/src/lib.rs
+++ kine-icu/src/lib.rs
@@ -1,5 +1,3 @@
-mod timezone;
-
 // Make cargo udeps happy for now
 pub fn todo() {
     let _date_iso = icu_calendar::Date::try_new_iso_date(1, 1, 1);
--- kine-icu/src/timezone.rs
+++
@@ -1,9 +0,0 @@
-/// A time zone
-pub trait TimeZone {
-    /// Write the designator of this timezone
-    ///
-    /// The designator is the string indicating which timezone a written date is in.
-    /// For instance, it is `"Z"` for UTC, `"+01:00"` for a fixed 1h offset, and
-    /// `" CET"` for Central European Time.
-    fn write_designator(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
-}
