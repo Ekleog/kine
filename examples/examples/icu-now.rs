6705153ff20c0cc06181c28e729bb88cfadcbc68
Léo Gaspard <leo@gaspard.io>
2023-02-06 02:02:21 +0100

    Add an example showing iso datetime formatting

---
+++ examples/examples/icu-now.rs
@@ -0,0 +1,13 @@
+use kine::{
+    icu::{self, cal::Iso},
+    tz::Utc,
+    Calendar, Time,
+};
+
+fn main() {
+    let now = Time::now();
+    let iso_utc = icu::Cal::new(Iso, Utc);
+    println!("Hello world! According to a few calendars (in UTC), we currently are:");
+    println!(" - {}", iso_utc.write(&now).unwrap().any());
+    // TODO: add an example with other calendars
+}
