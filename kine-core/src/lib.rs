9b4f2afbc1e6dea11769385153c3e6cc1b463323
Léo Gaspard <leo@gaspard.io>
2023-02-06 03:59:38 +0100

    Copy readme close to each crate for publishing

--- kine-core/src/lib.rs
+++ kine-core/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 
 mod calendar;
--- kine-icu/src/lib.rs
+++ kine-icu/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 
 use std::fmt::{self, Debug};
